35e4ad278efff276dd1a9592cf5bd7e9d31b15f0
Bilal <42200647+Bm3Smartboy@users.noreply.github.com>
2024-03-22 21:35:03 +0300

    Borrow fixed null pointer error

--- LibraryExceptions.java
+++ LibraryExceptions.java
@@ -1,17 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.concurrent.ExecutionException;
 
 public class LibraryExceptions extends Exception {
-    Information info;
-    ArrayList<Book> books;
-
-    public LibraryExceptions() throws Exception{
-       info =  new Information();
-        books = info.showAllBooks();
-    }
-
-
 
     public void checkQuantity(Book book) throws Exception {
         if (book.quantity == 0) {
@@ -24,6 +14,7 @@ public class LibraryExceptions extends Exception {
     public String setIsbn(Scanner sc,int Num) throws Exception{
         System.out.println("System.out.println(\"Enter the ISBN of the " + Num + " Book");
         String isbn = sc.nextLine();
+        ArrayList<Book> books = new Information().showAllBooks();
         for (Book x : books) {
             if(isbn.equals(x.ISBN)) {
                 checkQuantity(x);
